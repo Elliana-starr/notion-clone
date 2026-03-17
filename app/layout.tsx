1413a0ced0def7e265397c3464f03e86a3de674d
Elliana-starr <aderinsolamarvellous@gmail.com>
2026-03-17 11:57:30 +0100

    Use env variables for Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,3 @@
-import { ClerkProvider } from "@clerk/nextjs";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <ClerkProvider publishableKey={process.env.CLERK_PUBLISHABLE_KEY}>
-      {children}
-    </ClerkProvider>
-  );
-}
 
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
@@ -42,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
     <html lang="en" className={cn("font-sans", geist.variable)}>
       <body>
         <Header />
