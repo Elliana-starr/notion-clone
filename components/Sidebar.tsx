845e6e854b641eac4f084bfc93697e973930a13e
Elliana-starr <aderinsolamarvellous@gmail.com>
2026-03-18 12:25:16 +0100

    updated sidebar and added new document button

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,9 @@
+import NewDocumentButton from "./NewDocumentButton"
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className="p-2 md:p-5 bg-gray-200">
+      <NewDocumentButton />
+    </div>
   )
 }
 export default Sidebar
