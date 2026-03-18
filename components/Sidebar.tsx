f9804dbb68e01ba50485d0076302c6d322057c63
Elliana-starr <aderinsolamarvellous@gmail.com>
2026-03-18 21:16:28 +0100

    Rename project and add new features

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,49 @@
 import NewDocumentButton from "./NewDocumentButton"
+import { Menu as MenuIcon } from "lucide-react";
+
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 function Sidebar() {
-  return (
-    <div className="p-2 md:p-5 bg-gray-200">
+
+const menuOptions = (
+  <>
+     
       <NewDocumentButton />
+          
+  </>
+);
+
+  return (
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+      <div className="md:hidden">
+      <Sheet>
+  <SheetTrigger>
+    <MenuIcon className= "p-2 hover:opacity-30 rounded-lg" size={40}  >
+
+    </MenuIcon>
+  </SheetTrigger>
+  <SheetContent side="left">
+    <SheetHeader>
+      <SheetTitle>Menu</SheetTitle>
+      <div>{menuOptions}</div>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
+</div>
+      <div className="hidden md:inline">
+      {menuOptions}
+      </div>
+      
     </div>
   )
 }
